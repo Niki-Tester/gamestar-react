1f2c841ac1db1ed9941cdbd0a52c9e7a84739b30
Niki-Tester <bloatedniknak@gmail.com>
2023-02-22 11:40:28 +0000

    fix: update styles to now include PageContainer component

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,27 +1,30 @@
 import styles from "./NavBar.module.css";
 import logo from "./gamestar_logo_light.webp";
+import PageContainer from "../PageContainer/PageContainer";
 
 export const NavBar = () => {
   return (
     <header className={styles.main}>
-      <nav className="container">
-        <div className={styles.navLogo}>
-          <a href="/">
-            <img src={logo} alt="gamestar logo" height="52px" />
-          </a>
-        </div>
-        <ul className={styles.navLinks}>
-          <li>
-            <a href="/">Home</a>
-          </li>
-          <li>
-            <a href="/">Register</a>
-          </li>
-          <li>
-            <a href="/">Login</a>
-          </li>
-        </ul>
-      </nav>
+      <PageContainer>
+        <nav className={styles.navbar}>
+          <div className={styles.navLogo}>
+            <a href="/">
+              <img src={logo} alt="gamestar logo" height="52px" />
+            </a>
+          </div>
+          <ul className={styles.navLinks}>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/">Register</a>
+            </li>
+            <li>
+              <a href="/">Login</a>
+            </li>
+          </ul>
+        </nav>
+      </PageContainer>
     </header>
   );
 };
