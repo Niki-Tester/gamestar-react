b5d435bd99cd79cfb627f5814e9c1019dfe30941
Niki-Tester <bloatedniknak@gmail.com>
2023-02-22 09:31:31 +0000

    add home route

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import { Fragment } from "react";
 import "./App.css";
 import Background from "./Components/Background/Background";
 import { NavBar } from "./Components/NavBar/NavBar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./Pages/HomePage/HomePage";
 
 function App() {
   return (
     <Fragment>
-      <Background imageUrl={undefined} imageAlt={undefined} />
       <NavBar />
+      <Background imageUrl={undefined} imageAlt={undefined} />
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<HomePage />} />
+        </Routes>
+      </Router>
     </Fragment>
   );
 }
