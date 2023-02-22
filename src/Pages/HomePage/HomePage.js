d3cd1f0ac0cda44794fb657c6a44e08cb59d5735
Niki-Tester <bloatedniknak@gmail.com>
2023-02-22 12:47:26 +0000

    add game card grid to home page

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -1,9 +1,20 @@
 import logo from "./gamestar_full_logo.webp";
 import styles from "./HomePage.module.css";
 import PageContainer from "../../Components/PageContainer/PageContainer";
+import GameCardGrid from "../../Components/GameCardGrid/GameCardGrid";
+import GameCard from "../../Components/GameCard/GameCard";
 
 const HomePage = () => {
   document.title = "GameStar | Home";
+
+  const reviews = [
+    {
+      imageUrl:
+        "https://images.igdb.com/igdb/image/upload/t_cover_big/co4804.jpg",
+      name: "sailwind",
+      rating: 90,
+    },
+  ];
 
   return (
     <PageContainer>
@@ -11,6 +22,11 @@ const HomePage = () => {
         <img src={logo} alt="gamestar logo" />
         <p>Your place for video game reviews</p>
       </section>
+      <GameCardGrid>
+        {reviews.map((review) => (
+          <GameCard review={review} />
+        ))}
+      </GameCardGrid>
     </PageContainer>
   );
 };
