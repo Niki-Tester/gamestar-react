b7ec14ff60b970173977ce3ca8f2bcf165fd51e8
Niki-Tester <bloatedniknak@gmail.com>
2023-02-22 12:45:38 +0000

    feat: add home page

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -1,5 +1,18 @@
+import logo from "./gamestar_full_logo.webp";
+import styles from "./HomePage.module.css";
+import PageContainer from "../../Components/PageContainer/PageContainer";
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  document.title = "GameStar | Home";
+
+  return (
+    <PageContainer>
+      <section className={styles.main}>
+        <img src={logo} alt="gamestar logo" />
+        <p>Your place for video game reviews</p>
+      </section>
+    </PageContainer>
+  );
 };
 
 export default HomePage;
