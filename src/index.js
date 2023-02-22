288cd9136136b782845c38346c5839cdc6128dc8
Niki-Tester <bloatedniknak@gmail.com>
2023-02-22 09:30:58 +0000

    uninstall bootstrap, install react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { ThemeProvider } from "react-bootstrap";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
@@ -7,11 +6,6 @@ import "./index.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ThemeProvider
-      breakpoints={["xxxl", "xxl", "xl", "lg", "md", "sm", "xs", "xxs"]}
-      minBreakpoint="xxs"
-    >
-      <App />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>
 );
