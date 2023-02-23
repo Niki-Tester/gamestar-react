f112efb7caacc8686b4ed8224b58b0a9e7ec7909
Niki-Tester <bloatedniknak@gmail.com>
2023-02-23 16:58:28 +0000

    feat: display loader/gamecards

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -3,31 +3,33 @@ import styles from "./HomePage.module.css";
 import PageContainer from "../../Components/PageContainer/PageContainer";
 import GameCardGrid from "../../Components/GameCardGrid/GameCardGrid";
 import GameCard from "../../Components/GameCard/GameCard";
+import useFetch from "../../Hooks/useFetch";
+import LoadingSpinner from "../../Components/LoadingSpinner/LoadingSpinner";
 
 const HomePage = () => {
   document.title = "GameStar | Home";
-
-  const reviews = [
-    {
-      id: 1,
-      imageUrl:
-        "https://images.igdb.com/igdb/image/upload/t_cover_big/co4804.jpg",
-      name: "sailwind",
-      rating: 90,
-    },
-  ];
-
+  const endpoint = "games/";
+  const { data: games, loading, error } = useFetch(endpoint);
   return (
     <PageContainer>
       <section className={styles.main}>
         <img src={logo} alt="gamestar logo" />
         <p>Your place for video game reviews</p>
       </section>
-      <GameCardGrid>
-        {reviews.map((review) => (
-          <GameCard review={review} key={review.id} />
-        ))}
-      </GameCardGrid>
+      {loading && <LoadingSpinner />}
+      {games && (
+        <GameCardGrid>
+          {games
+            ? games.map((game) => {
+                if (game.cover_art === "/static/images/no_cover.webp") {
+                  game.cover_art = undefined;
+                }
+                return <GameCard game={game} key={game.id} />;
+              })
+            : "Loading Games..."}
+        </GameCardGrid>
+      )}
+      {error && console.log(error)}
     </PageContainer>
   );
 };
