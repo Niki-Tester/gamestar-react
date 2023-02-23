62b62c67c754893028f02cdedbc39a3c575064c5
Niki-Tester <bloatedniknak@gmail.com>
2023-02-23 23:29:32 +0000

    add AbortController

--- src/Hooks/useFetch.js
+++ src/Hooks/useFetch.js
@@ -6,20 +6,26 @@ const useFetch = (endpoint) => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
+    const abortController = new AbortController();
     setLoading(true);
     setData(null);
     setError(null);
 
-    fetch(`https://gamestar-react-server.onrender.com/${endpoint}`)
+    fetch(`https://gamestar-react-server.onrender.com/${endpoint}`, {
+      signal: abortController.signal,
+    })
       .then((res) => res.json())
       .then((data) => {
         setLoading(false);
         setData(data);
       })
       .catch((err) => {
+        if (err.name === "AbortError") return;
         setLoading(false);
         setError(err);
       });
+
+    return () => abortController.abort();
   }, [endpoint]);
   return { data, loading, error };
 };
