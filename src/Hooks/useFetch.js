8ae78187dde6c29b4bc7398db41f59b3f558d2e6
Niki-Tester <bloatedniknak@gmail.com>
2023-02-23 23:21:21 +0000

    use live express server

--- src/Hooks/useFetch.js
+++ src/Hooks/useFetch.js
@@ -10,7 +10,7 @@ const useFetch = (endpoint) => {
     setData(null);
     setError(null);
 
-    fetch(`http://127.0.0.1:9554/${endpoint}`)
+    fetch(`https://gamestar-react-server.onrender.com/${endpoint}`)
       .then((res) => res.json())
       .then((data) => {
         setLoading(false);
