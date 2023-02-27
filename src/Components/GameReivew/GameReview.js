3f71ab541609dcf9919b290abf52bef853f5e3cf
Niki-Tester <bloatedniknak@gmail.com>
2023-02-27 11:27:02 +0000

    feat: add username and likes to summary

--- src/Components/GameReivew/GameReview.js
+++ src/Components/GameReivew/GameReview.js
@@ -1,12 +1,27 @@
+import useFetch from "../../Hooks/useFetch";
 import GameRating from "../GameRating/GameRating";
+import SmallLoadingSpinner from "../SmallLoadingSpinner/SmallLoadingSpinner";
 import styles from "./GameReview.module.css";
 
 const GameReview = ({ review }) => {
+  const { data: user, loading, error } = useFetch(`user/${review.user_id}`);
   return (
     <div className={styles.main}>
       <h3 className={styles.heading}>{review.heading}</h3>
       <div className={styles.info}>
         <GameRating gameId={review.game_id} />
+        <div className={styles.likes}>
+          <i className="fa-regular fa-heart"></i>
+          <span>{review.likes.length}</span>
+        </div>
+        {loading && <SmallLoadingSpinner />}
+        {user && (
+          <p>
+            <i className="fa-regular fa-user"></i>{" "}
+            <span className={styles.username}>{user.username}</span>
+          </p>
+        )}
+        {error && console.error(error.message)}
       </div>
     </div>
   );
