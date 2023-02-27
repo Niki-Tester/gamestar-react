c6ee5a2dd6bd5ca541fc4b6a1137fc0e377bb1b2
Niki-Tester <bloatedniknak@gmail.com>
2023-02-27 11:37:10 +0000

    set fetch url to production

--- src/Hooks/useFetch.js
+++ src/Hooks/useFetch.js
@@ -11,7 +11,7 @@ const useFetch = (endpoint) => {
     setData(null);
     setError(null);
 
-    fetch(`http://127.0.0.1:9554/${endpoint}`, {
+    fetch(`https://gamestar-react-server.onrender.com/${endpoint}`, {
       signal: abortController.signal,
     })
       .then((res) => res.json())
